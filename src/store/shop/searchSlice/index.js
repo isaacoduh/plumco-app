c17342ea0ffccf103b351d14acbe7c2b37c3bd95
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-31 15:31:33 +0100

    review and search slice

---
+++ src/store/shop/searchSlice/index.js
@@ -0,0 +1,43 @@
+import { BASE_API_URL } from "@/lib/constants";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  searchResults: [],
+};
+
+export const getSearchResults = createAsyncThunk(
+  "/order/getSearchResults",
+  async (keyword) => {
+    const response = await axios.get(`${BASE_API_URL}/search/${keyword}`);
+
+    return response.data;
+  }
+);
+
+const searchSlice = createSlice({
+  name: "searchSlice",
+  initialState,
+  reducers: {
+    resetSearchResults: (state) => {
+      state.searchResults = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSearchResults.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getSearchResults.fulfilled, (state, action) => {
+        (state.isLoading = false), (state.searchResults = action.payload.data);
+      })
+      .addCase(getSearchResults.rejected, (state) => {
+        state.isLoading = false;
+        state.searchResults = [];
+      });
+  },
+});
+
+export const { resetSearchResults } = searchSlice.actions;
+export default searchSlice.reducer;
